cb5c7c353953ed26e10cdce576a8facaf6eb64ba
ecaterina99 <tricolicikatea1999@gmail.com>
2024-12-05 14:12:50 +0200

    circle volume

---
+++ CircleVolume/src/circleVolume.java
@@ -0,0 +1,8 @@
+public class circleVolume {
+    public static void main(String[] args) {
+        double PI = 3.14;
+        double r = 10;
+        double v = (4/3) * PI * (r*r*r);
+        System.out.printf("Circle volume: %.2f%n", v);
+    }
+}
