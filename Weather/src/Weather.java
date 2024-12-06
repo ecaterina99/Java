46d490d088c4de8c2e9048ae77313359dbffc6f6
ecaterina99 <tricolicikatea1999@gmail.com>
2024-12-07 00:02:58 +0200

    Weather (if_else)

---
+++ Weather/src/Weather.java
@@ -0,0 +1,10 @@
+public class Weather {
+    public static void main(String[] args) {
+        boolean rain = false, cloudy = false;
+        if (rain || cloudy) {
+            System.out.println("The weather is bad outside.");
+        } else {
+            System.out.println("The weather is good.");
+        }
+    }
+}
