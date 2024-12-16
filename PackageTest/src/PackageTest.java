c1f4020900c04a5a2cae92daca91cb009cb9103f
ecaterina99 <tricolicikatea1999@gmail.com>
2024-12-16 12:19:29 +0200

    Package test

---
+++ PackageTest/src/PackageTest.java
@@ -0,0 +1,13 @@
+import Forest.Squirrel;
+//import Forest.Tree;
+import java.util.Scanner;
+
+//import Forest.*;
+
+public class PackageTest {
+    public static void main(String[] args) {
+        Squirrel s1 = new Squirrel();
+        Forest.Tree tree = new Forest.Tree();
+        Scanner sc = new Scanner(System.in);
+    }
+}
