61ac1c4f7a011a13fab4a8b1eb76bb54c252c7ad
ecaterina99 <tricolicikatea1999@gmail.com>
2024-12-26 12:41:35 +0200

    ReplaceAll

---
+++ ReplaceAll/src/ReplaceAll.java
@@ -0,0 +1,9 @@
+/*
+Write a function that removes the spaces from the string, then return the resultant string.
+ */
+
+public class ReplaceAll {
+    public static String noSpace(final String x) {
+        return x.replaceAll("\\s", "");
+    }
+}
