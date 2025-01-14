a2ab93e72de38bf625959d494b36251983aed6a9
ecaterina99 <tricolicikatea1999@gmail.com>
2025-01-14 12:46:34 +0200

    Update Animal test ( Upcasting & Downcasting)

--- AnimalTest/src/AnimalTest.java
+++ AnimalTest/src/AnimalTest.java
@@ -9,11 +9,22 @@ public class AnimalTest {
 
         Animal animal = new Animal();
         Cat cat = new Cat();
+        Animal animal2 = new Dog(); // Upcasting - Este conversia unui obiect al unei clase copil la tipul clasei părinte.
+        animal2.eat();
 
+//         Downcasting - Este conversia unui obiect al clasei părinte la tipul clasei copil. Necesită specificarea explicită a tipului.
+        Dog dog2 = (Dog) animal2;
+        dog2.sleep();
+
+
+        animal.sleep();
         test(animal);
         test(cat);
         test(rex);
+        test(animal2);
+        test(dog2);
     }
+
     public static void test(Animal animal) {
         animal.eat();
     }
