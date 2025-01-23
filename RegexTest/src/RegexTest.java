275b264c7ed4030b8d250af6ecb94a9ad2b7f64c
ecaterina99 <tricolicikatea1999@gmail.com>
2025-01-23 13:16:18 +0200

    Update RegexTest.java

--- RegexTest/src/RegexTest.java
+++ RegexTest/src/RegexTest.java
@@ -34,19 +34,38 @@ public class RegexTest {
         Formatul valid al numărului de telefon trebuie să fie: 000/123-456
         */
 
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Please enter your phone number");
-        String phone = scanner.nextLine();
-        boolean match = phone.matches("[0-9]{3}/[0-9]{3}-[0-9]{3}");
-        if (match) {
-            System.out.println("Your phone number is valid");
-        }
-        else {
-            System.out.println("Your phone number is not valid");
-        }
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Please enter your phone number");
+//        String phone = scanner.nextLine();
+//        boolean match = phone.matches("[0-9]{3}/[0-9]{3}-[0-9]{3}");
+//        if (match) {
+//            System.out.println("Your phone number is valid");
+//        } else {
+//            System.out.println("Your phone number is not valid");
+//        }
 
 
-        
+        /*
+        În aplicaţie apar date despre studenţi. Formatul unor astfel de date este următorul:
+        John Torrance - 66/35
+        Este necesar să scrieți o expresie regulată și logica însoțitoare,
+        care va verifica dacă datele care sosesc sunt valide, adică în forma tocmai prezentată.
+         */
+
+        String student1 = "John Torrance - 66/35";
+        String student2 = "Anna Barn 66 35";
+        String student3 = "Michael Bing - 88/21";
+
+        String regex2 = "[A-Z][a-z]+\\s+[A-Z][a-z]+\\s-\\s[0-9]{2}/[0-9]{2}";
+
+        boolean valid = Pattern.matches(regex2, student1);
+        boolean valid2 = Pattern.matches(regex2, student2);
+        boolean valid3 = Pattern.matches(regex2, student3);
+
+        System.out.println(student1 + " " + valid);
+        System.out.println(student2 + " " + valid2);
+        System.out.println(student3 + " " + valid3);
+
 
     }
 }
