a7dafe7ec85e6f0a04be847afa21275ee52a6d22
ecaterina99 <tricolicikatea1999@gmail.com>
2025-02-21 17:26:17 +0200

    add delete method

--- JosephusProblem/src/JosephusProblem.java
+++ JosephusProblem/src/JosephusProblem.java
@@ -1,129 +1,114 @@
 public class JosephusProblem {
-    private Node head;
 
     public static void main(String[] args) {
 
-
-        JosephusProblem circle = new JosephusProblem();
-
+        LinkedList circle = new LinkedList();
+        int step = 2;
         circle.insert(1);
-        circle.insert(2);//
+        circle.insert(2);
         circle.insert(3);
-        circle.insert(4);///
+        circle.insert(4);
         circle.insert(5);
-        circle.insert(6);//
+        circle.insert(6);
         circle.insert(7);
+
         circle.display();
         System.out.println("Total numbers: " + circle.countNodes());
-        int step = 2;
-        circle.deleteSequentially(step);
-
-
+        System.out.println("Last remaining: ");
+        circle.delete(step);
     }
 
-    class Node {
-        int data;
-        Node next;
-        Node prev;
+    public static class LinkedList {
+        private Node head;
 
-        public Node(int data) {
-            this.data = data;
-            this.next = null;
-            this.prev = null;
-        }
-    }
+        public class Node {
+            int data;
+            Node next;
+            Node prev;
 
-    public void insert(int data) {
-        Node newNode = new Node(data);
-        if (head == null) {
-            head = newNode;
-            newNode.prev = head;
-            newNode.next = head;
-        } else {
-            Node last = head.prev;
-            last.next = newNode;
-            newNode.prev = last;
-            newNode.next = head;
-            head.prev = newNode;
-
-        }
-    }
-
-    public void display() {
-        if (head == null) {
-            System.out.println("The list is empty!");
-            return;
-        }
-        Node current = head;
-        do {
-            System.out.print(current.data + " ");
-            current = current.next;
-        } while (current != head);
-        System.out.println();
-    }
-
-    public void delete(int key) {
-        if (head == null) {
-            System.out.println("The list is empty!");
-            return;
+            public Node(int data) {
+                this.data = data;
+                this.next = null;
+                this.prev = null;
+            }
         }
 
-        Node current = head;
-        while (current.data != key) {
-            if (current.next == head) {
-                System.out.println("The element is not in the list");
+        public void insert(int data) {
+            Node newNode = new Node(data);
+            if (head == null) {
+                head = newNode;
+                newNode.prev = head;
+                newNode.next = head;
+            } else {
+                Node last = head.prev;
+                last.next = newNode;
+                newNode.prev = last;
+                newNode.next = head;
+                head.prev = newNode;
+            }
+        }
+
+        public void display() {
+            if (head == null) {
+                System.out.println("The list is empty!");
                 return;
             }
-            current = current.next;
-        }
-        if (current == head && current.next == head) {
-            head = null;
-        } else if (current == head) {
-            head = head.next;
-            head.prev = head.next;
-            current.prev.next = head;
-        } else {
-            current.prev.next = current.next;
-            current.next.prev = current.prev;
-        }
-    }
-
-    public int countNodes() {
-        if (head == null) {
-            return 0;
-        }
-        int count = 0;
-        Node current = head;
-
-        do {
-            current = current.next;
-            count++;
-        } while (current != head);
-
-        return count;
-    }
-
-    public void deleteSequentially(int step) {
-        if (head == null) {
-            System.out.println("The list is empty!");
-            return;
-        }
-
-        Node current = head;
-        int currentPosition = 0;
+            Node current = head;
             do {
-                currentPosition++;
+                System.out.print(current.data + " ");
                 current = current.next;
-                if(currentPosition % step == 0) {
-                    System.out.print(current.data + " ");
-                    current.prev.next = current.next;
-                    current.next.prev = current.prev;
-                }
-            }
-            while (current.next != current.prev);
-
-
-
+            } while (current != head);
+            System.out.println();
         }
+
+        public int countNodes() {
+            if (head == null) {
+                return 0;
+            }
+            int count = 0;
+            Node current = head;
+            do {
+                current = current.next;
+                count++;
+            } while (current != head);
+            return count;
+        }
+
+        public void delete(int step) {
+            if (head == null) return;
+
+            Node current = head;
+            int size = countNodes();
+
+            while (size > 1) {
+                for (int i = 0; i < step - 1; i++) {
+                    current = current.next;
+                }
+
+                System.out.println("Removing: " + current.data);
+
+                //only 1 node in the list
+                if (current.next == current) {
+                    head = null;
+                    return;
+                }
+
+                // delete node
+                current.prev.next = current.next;
+                current.next.prev = current.prev;
+
+                // delete head
+                if (current == head) {
+                    head = current.next;
+                }
+
+                current = current.next;
+                size--;
+            }
+
+            System.out.println("The winner: " + current.data);
+        }
+
     }
+}
 
