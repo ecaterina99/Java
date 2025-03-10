16d179b656fc9600ed4899ab9f33371a0e67e414
ecaterina99 <tricolicikatea1999@gmail.com>
2025-03-10 19:42:59 +0200

    efficient searching

---
+++ HowManyStreets/src/HowManyStreets.java
@@ -0,0 +1,77 @@
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+public class HowManyStreets {
+
+
+    public static int[] countStreets(String[] streets, String[][] drivers) {
+        int n = streets.length;
+        int d = drivers.length;
+        int[] result = new int[d];
+
+        Map<String, Integer> streetIndex = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            streetIndex.put(streets[i], i);
+            System.out.println(streets[i]);
+        }
+
+        for (int j = 0; j < d; j++) {
+            int start = streetIndex.get(drivers[j][0]);
+            int end = streetIndex.get(drivers[j][1]);
+
+            result[j] = Math.abs(end - start) - 1;
+            System.out.println(start + " " + end + " = " + result[j]);
+        }
+
+        return result;
+    }
+
+            public static void main(String[] args) {
+                final String[] streets = {"first", "second", "third", "fourth", "fifth", "sixth", "seven"};
+                final String[][] drivers = {{"first", "second"}, {"second", "seven"}, {"sixth", "fourth"}};
+
+
+                System.out.println(Arrays.toString(countStreets(streets, drivers)));
+            }
+}
+
+
+
+
+
+  /*     int rows = drivers.length;
+
+        int result = 0;
+        int[] resArray = new int[rows];
+
+
+        for (int i = 0; i < streets.length; i++) {
+            for (int j = 0; j < rows; j++) {
+                String target1 = drivers[j][0];
+
+
+                if (streets[i].equals(target1)) {
+                    String target2 = drivers[j][1];
+                    for (int k = 0; k < streets.length; k++) {
+                        if (streets[k].equals(target2)) {
+                            result = k - i;
+                            if (result > 0) {
+                                result = result - 1;
+                            } else {
+                                result = -1 - (result);
+                            }
+                        }
+                        resArray[j] = result;
+                    }
+
+                }
+            }
+        }
+        System.out.println(Arrays.toString(resArray));
+    }
+
+}
+
+*/
