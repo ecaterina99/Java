17e838fba7f40275fe5c60ec8bf811e357b8fc93
ecaterina99 <tricolicikatea1999@gmail.com>
2025-03-24 22:49:55 +0200

    Binary Search Tree

---
+++ BinarySearcTreeTest/src/BinarySearchTreeTest.java
@@ -0,0 +1,22 @@
+
+public class BinarySearchTreeTest {
+    public static void main(String[] args) {
+        BinarySearchTree tree = new BinarySearchTree();
+
+        tree.insert(new Node(5));
+        tree.insert(new Node(8));
+        tree.insert(new Node(4));
+        tree.insert(new Node(2));
+        tree.insert(new Node(7));
+        tree.insert(new Node(3));
+        tree.insert(new Node(6));
+        tree.insert(new Node(9));
+        tree.insert(new Node(1));
+        tree.insert(new Node(10));
+
+        tree.remove(7);
+        System.out.println(tree.search(1));
+        tree.display();
+
+    }
+}
