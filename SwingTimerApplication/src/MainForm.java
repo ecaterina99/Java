f86c0eda4572d5d20a5a355dd89a2d1e492e2d9a
ecaterina99 <tricolicikatea1999@gmail.com>
2025-04-15 09:56:32 +0300

    create functions: initializeSpeedChooser, initializeControlButtons , initializeColorChooser, initializeTimerOptions, startTimer, startBlinkingWindow

--- SwingTimerApplication/src/MainForm.java
+++ SwingTimerApplication/src/MainForm.java
@@ -1,35 +1,31 @@
 import javax.swing.*;
-import javax.swing.text.DefaultFormatterFactory;
-import javax.swing.text.MaskFormatter;
-import javax.swing.text.NumberFormatter;
+import javax.swing.text.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.text.NumberFormat;
-import java.text.ParseException;
+import java.awt.event.*;
+import java.text.*;
+import java.time.*;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
+import java.util.Timer;
+
 
 public class MainForm extends JFrame {
 
-    private JPanel mainPanel;
-    private JPanel settingsPanel;
-
-    private JPanel timePanel;
-    private JPanel countdownPanel;
-    private JRadioButton onTime;
-    private JRadioButton countdown;
-    private JFormattedTextField onTimeField;
-    private JFormattedTextField countdownField;
-
-    private JPanel StartCancelPanel;
-    private JButton STARTButton;
-    private JButton CANCELButton;
-
-    private JPanel colorPanel;
-    private JButton ColorChooser;
-
-    private JPanel speedChooser;
-    private JLabel SpeedLabel;
-    private JComboBox speed;
+    private JPanel mainPanel, timerOptionsPanel, onTimePanel, countdownPanel, controlButtonsPanel, colorPanel, speedChooserPanel;
+    private JRadioButton onTimeRadioButton, countdownRadioButton;
+    private JFormattedTextField onTimeField, countdownField;
+    private JButton startButton, stopButton, colorChooser;
+    private Color selectedColor = Color.WHITE;
+    private JLabel speedLabel;
+    private JComboBox<Integer> blinkSpeedComboBox;
+    private Timer timer;
+    private JFrame blinkingFrame;
+    private boolean isBlinking = false;
+    private static final String TITLE_APPLICATION = "Timer Application";
+    private static final String TITLE_BLINKING_FRAME = "Blinking Window";
+    private static final Dimension DEFAULT_SIZE = new Dimension(300, 300);
+    private static final String[] DIALOG_OPTIONS = {"Settings", "Close"};
+    private static final String TIME_MASK = "##:##:##";
 
 
     public MainForm() {
@@ -43,43 +39,37 @@ public class MainForm extends JFrame {
     }
 
     private void showOptionDialog() {
-        int optionDialog = JOptionPane.showOptionDialog(null, "Choose option", "Option dialog",
-                JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE, null, new String[]{"Settings", "Close"}, null);
-
-        switch (optionDialog) {
-            case JOptionPane.YES_OPTION:
-                StartTimer();
-                break;
-            case JOptionPane.NO_OPTION:
-                JOptionPane.showMessageDialog(null, "Application will close");
-                System.exit(0);
-                break;
-            case -1:
-                JOptionPane.showMessageDialog(null, "You have to choose an option!");
-                break;
+        int choice = JOptionPane.showOptionDialog(null, "Choose option", "Option dialog",
+                JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE, null, DIALOG_OPTIONS, null);
+        if (choice == JOptionPane.YES_OPTION) {
+            initializeComponents();
         }
     }
 
-    private void StartTimer() {
-        setFrameProperties();
-        selectOption();
-        chooseColor();
-        chooseSpeed();
-        selectStartOrCancel();
+    private void initializeComponents() {
+        showSettingsWindow();
+        initializeTimerOptions();
+        initializeColorChooser();
+        initializeSpeedChooser();
+        initializeControlButtons();
+    }
+
+    public void showSettingsWindow() {
+        this.setTitle(TITLE_APPLICATION);
+        this.setSize(DEFAULT_SIZE);
+        this.setLocationRelativeTo(null);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setLayout(new BoxLayout(this.getContentPane(), BoxLayout.Y_AXIS));
+        this.setVisible(true);
     }
 
 
-    public void setFrameProperties() {
-        JFrame settingsFrame = new JFrame("Settings");
-        settingsFrame.setSize(500, 300);
-        settingsFrame.setLocationRelativeTo(null);
-        settingsFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        settingsPanel.setLayout(new BoxLayout(settingsPanel, BoxLayout.Y_AXIS));
-        settingsFrame.setContentPane(settingsPanel);
-        settingsFrame.setVisible(true);
-    }
+    public void initializeTimerOptions() {
 
-    public void selectOption() {
+        ButtonGroup timerOptions = new ButtonGroup();
+        timerOptions.add(onTimeRadioButton);
+        timerOptions.add(countdownRadioButton);
+
         ActionListener osListener = new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 if (e.getActionCommand().equals("onTime")) {
@@ -93,99 +83,207 @@ public class MainForm extends JFrame {
             }
         };
 
+        onTimeRadioButton.setActionCommand("onTime");
+        onTimeRadioButton.addActionListener(osListener);
+        onTimeRadioButton.setSelected(true);
+        onTimePanel.setLayout(new FlowLayout(FlowLayout.CENTER));
+        onTimePanel.add(onTimeRadioButton);
+        onTimePanel.add(onTimeField);
 
-        ButtonGroup myChoices = new ButtonGroup();
+        countdownRadioButton.setActionCommand("countdown");
+        countdownRadioButton.addActionListener(osListener);
+        countdownPanel.setLayout(new FlowLayout(FlowLayout.CENTER));
+        countdownPanel.add(countdownRadioButton);
+        countdownPanel.add(countdownField);
 
-        onTimeField.setColumns(10);
-        onTime.setActionCommand("onTime");
-        onTime.addActionListener(osListener);
-        myChoices.add(onTime);
-        onTime.setSelected(true);
-        onTimeField.setFormatterFactory(new JFormattedTextField.AbstractFormatterFactory() {
+        configureTimeFormatters();
+
+        timerOptionsPanel.add(onTimePanel);
+        timerOptionsPanel.add(countdownPanel);
+        this.add(timerOptionsPanel);
+    }
+
+    private void configureTimeFormatters() {
+        try {
+            MaskFormatter timeFormatter = new MaskFormatter(TIME_MASK);
+            timeFormatter.setPlaceholderCharacter('_');
+            onTimeField.setFormatterFactory(new DefaultFormatterFactory(timeFormatter));
+            onTimeField.setColumns(6);
+
+            NumberFormatter countdownFormatter = new NumberFormatter(NumberFormat.getIntegerInstance());
+            countdownFormatter.setValueClass(Integer.class);
+            countdownFormatter.setMinimum(1);
+            countdownFormatter.setMaximum(60);
+            countdownFormatter.setAllowsInvalid(false);
+            countdownField.setFormatterFactory(new DefaultFormatterFactory(countdownFormatter));
+            countdownField.setColumns(6);
+            countdownField.setEnabled(false);
+        } catch (ParseException e) {
+            handleFormatException(e);
+        }
+    }
+
+    private void handleFormatException(Exception e) {
+        JOptionPane.showMessageDialog(this, "Error configuring time format: " + e.getMessage(),
+                "Format Error", JOptionPane.ERROR_MESSAGE);
+    }
+
+    public void initializeColorChooser() {
+        colorPanel.setLayout(new FlowLayout(FlowLayout.CENTER));
+        colorChooser.addActionListener(new ActionListener() {
             @Override
-            public JFormattedTextField.AbstractFormatter getFormatter(JFormattedTextField tf) {
-                try {
-                    MaskFormatter formatter = new MaskFormatter("##:##:##");
-                    formatter.setPlaceholderCharacter('_');
-                    return formatter;
-                } catch (ParseException e) {
-                    e.printStackTrace();
-                    return null;
+            public void actionPerformed(ActionEvent e) {
+                Color color = JColorChooser.showDialog(null, "Choose background color", selectedColor);
+                if (color != null) {
+                    selectedColor = color;
+                    colorPanel.setForeground(color);
                 }
             }
         });
-        timePanel.setLayout(new FlowLayout(FlowLayout.LEFT));
-        timePanel.add(onTime);
-        timePanel.add(onTimeField);
-        settingsPanel.add(timePanel);
+        colorPanel.add(colorChooser);
+        this.add(colorPanel);
+    }
 
-        countdown.setActionCommand("countdown");
-        countdown.addActionListener(osListener);
-        myChoices.add(countdown);
-        NumberFormatter formatter = new NumberFormatter(NumberFormat.getIntegerInstance());
-        formatter.setValueClass(Integer.class);
-        formatter.setMinimum(0);
-        formatter.setMaximum(60);
-        formatter.setAllowsInvalid(false);
-        countdownField.setFormatterFactory(new DefaultFormatterFactory(formatter));
-        countdownField.setColumns(10);
+    public void initializeSpeedChooser() {
+        speedChooserPanel.setLayout(new FlowLayout(FlowLayout.CENTER));
+        speedChooserPanel.add(speedLabel);
+        speedChooserPanel.add(blinkSpeedComboBox);
+        this.add(speedChooserPanel);
+    }
+
+    public void initializeControlButtons() {
+        controlButtonsPanel.setLayout(new FlowLayout(FlowLayout.CENTER));
+
+        startButton.addActionListener(e -> {
+            disableControls();
+            startTimer();
+        });
+        stopButton.addActionListener(e -> {
+            stopTimer();
+            if (blinkingFrame != null) blinkingFrame.dispose();
+            enableControls();
+        });
+        stopButton.setEnabled(false);
+        controlButtonsPanel.add(startButton);
+        controlButtonsPanel.add(stopButton);
+        this.add(controlButtonsPanel);
+    }
+
+    private void disableControls() {
+        onTimeRadioButton.setEnabled(false);
+        countdownRadioButton.setEnabled(false);
+        onTimeField.setEnabled(false);
         countdownField.setEnabled(false);
-        countdownPanel.setLayout(new FlowLayout(FlowLayout.LEFT));
-        countdownPanel.add(countdown);
-        countdownPanel.add(countdownField);
-        settingsPanel.add(countdownPanel);
+        colorChooser.setEnabled(false);
+        blinkSpeedComboBox.setEnabled(false);
+        startButton.setEnabled(false);
+        stopButton.setEnabled(true);
     }
 
-    public void chooseColor() {
-        ColorChooser.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                openNewWindow();
-            }
-        });
-        colorPanel.setLayout(new FlowLayout(FlowLayout.CENTER));
-        colorPanel.add(ColorChooser);
-        settingsPanel.add(colorPanel);
+    private void enableControls() {
+        onTimeRadioButton.setEnabled(true);
+        countdownRadioButton.setEnabled(true);
+        onTimeField.setEnabled(onTimeRadioButton.isSelected());
+        countdownField.setEnabled(countdownRadioButton.isSelected());
+        colorChooser.setEnabled(true);
+        blinkSpeedComboBox.setEnabled(true);
+        startButton.setEnabled(true);
+        stopButton.setEnabled(false);
     }
 
-    public void chooseSpeed() {
-        speedChooser.setLayout(new FlowLayout(FlowLayout.LEFT));
-        speedChooser.add(SpeedLabel);
-        speedChooser.add(speed);
-        settingsPanel.add(speedChooser);
-    }
+    private void startTimer() {
+        if (timer != null) timer.cancel();
+        timer = new Timer();
 
-    public void selectStartOrCancel() {
-        StartCancelPanel.setLayout(new FlowLayout(FlowLayout.CENTER));
-        StartCancelPanel.add(STARTButton);
-        StartCancelPanel.add(CANCELButton);
-
-        CANCELButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            }
-        });
-        settingsPanel.add(StartCancelPanel);
-    }
-
-    private void openNewWindow() {
-        JFrame colorFrame = new JFrame("Rainbow!");
-        colorFrame.setSize(300, 300);
-        Color backgroundColor = JColorChooser.showDialog(null, "Choose background color", Color.pink);
-        if (backgroundColor != null) {
-            colorFrame.getContentPane().setBackground(backgroundColor);
+        long delay = 0;
+        if (selectedColor == null || selectedColor == Color.white) {
+            JOptionPane.showMessageDialog(null, "Please choose a color before starting! (Not white)");
+            enableControls();
+            return;
         }
-        colorFrame.setLocationRelativeTo(null);
-        colorFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE );
-        settingsPanel.setLayout(new BoxLayout(settingsPanel, BoxLayout.Y_AXIS));
-        colorFrame.setVisible(true);
+        if (countdownRadioButton.isSelected()) {
+            try {
+                delay = ((Number) countdownField.getValue()).longValue() * 1000;
+            } catch (Exception ex) {
+                JOptionPane.showMessageDialog(null, "Please enter a valid number of seconds!");
+                enableControls();
+                return;
+            }
+        } else {
+            try {
+                LocalTime setTime = LocalTime.parse(onTimeField.getText(), DateTimeFormatter.ofPattern("HH:mm:ss"));
+                LocalTime now = LocalTime.now();
+                long setTimeSeconds = setTime.toSecondOfDay();
+                long nowSeconds = now.toSecondOfDay();
+                if (setTimeSeconds <= nowSeconds) {
+                    delay = (24 * 60 * 60 - nowSeconds + setTimeSeconds) * 1000;
+                } else {
+                    delay = (setTimeSeconds - nowSeconds) * 1000;
+                }
+            } catch (Exception ex) {
+                JOptionPane.showMessageDialog(null, "Incorrect time format!");
+                enableControls();
+                return;
+            }
+        }
+
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                SwingUtilities.invokeLater(() -> startBlinkingWindow());
+            }
+        }, delay);
     }
 
+    private void stopTimer() {
+        if (timer != null) {
+            timer.cancel();
+        }
+        isBlinking = false;
+    }
+
+    private void startBlinkingWindow() {
+        blinkingFrame = new JFrame(TITLE_BLINKING_FRAME);
+        blinkingFrame.setSize(DEFAULT_SIZE);
+        blinkingFrame.setLocation(
+                this.getX() + this.getWidth() + 10, this.getY()
+        );
+
+        blinkingFrame.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                isBlinking = false;
+                enableControls();
+            }
+        });
+
+        blinkingFrame.setVisible(true);
+
+        Object selectedInterval = blinkSpeedComboBox.getSelectedItem();
+        int blinkingInterval = Integer.parseInt(selectedInterval.toString()) * 1000;
+
+        isBlinking = true;
+
+        Timer blinkTimer = new Timer();
+        blinkTimer.scheduleAtFixedRate(new TimerTask() {
+            boolean toggle = true;
+
+            @Override
+            public void run() {
+                if (!isBlinking) {
+                    blinkTimer.cancel();
+                    return;
+                }
+                SwingUtilities.invokeLater(() -> {
+                    blinkingFrame.getContentPane().setBackground(toggle ? selectedColor : Color.WHITE);
+                    toggle = !toggle;
+                });
+            }
+        }, 0, blinkingInterval);
+    }
 
     public static void main(String[] args) {
         MainForm frame = new MainForm();
-        frame.setVisible(false);
     }
 }
 
