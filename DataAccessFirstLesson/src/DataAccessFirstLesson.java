5985897d1315853588e49ea76eeda0dac01c226d
ecaterina99 <tricolicikatea1999@gmail.com>
2025-05-02 11:58:32 +0300

    Create data base connection

--- DataAccessFirstLesson/src/DataAccessFirstLesson.java
+++ DataAccessFirstLesson/src/DataAccessFirstLesson.java
@@ -1,6 +1,7 @@
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.sql.*;
 
 public class DataAccessFirstLesson {
     public static void main(String[] args) throws IOException {
@@ -9,25 +10,47 @@ public class DataAccessFirstLesson {
         fw.write(person.toString() + "\n");
         fw.close();
     }
-         */
-        Person person = null;
 
+        Person person = null;
         FileReader fr = new FileReader("myFile.txt");
         StringBuilder stringBuilder = new StringBuilder();
-
         int ch;
         while ((ch = fr.read()) != -1) {
             stringBuilder.append((char) ch);
         }
-
         String userData = stringBuilder.toString();
         String[] userProps = userData.split("\n");
-
         if (userProps.length == 3) {
             person = new Person(userProps[0], Integer.valueOf(userProps[1]), userProps[2]);
         }
-
         System.out.println(person);
+
+         */
+
+
+        Person person = new Person("Bob Ross", 33);
+        try (Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/test", "root", "123123");
+        ) {
+            System.out.println("Connected to database ");
+
+            // Use interface statement
+            /*
+            Statement st = conn.createStatement();
+            String query = "insert into persons values(null,'" + person.getName() + "', " + person.getAge() + ")";
+            st.execute(query);
+             */
+
+            String query =  "insert into persons (name, age) values(?, ?)";
+            PreparedStatement ps = conn.prepareStatement(query);
+            ps.setString(1, person.getName());
+            ps.setInt(2, person.getAge());
+            ps.execute();
+
+
+        } catch (SQLException e) {
+            System.out.println("Error in database connection" + e.getMessage());
+        }
+
     }
 
 }
--- DataAccessFirstLesson/src/Person.java
+++ DataAccessFirstLesson/src/Person.java
@@ -2,25 +2,33 @@ public class Person {
 
     private String name;
     private int age;
-    private String pid;
 
-    public String getName() {       return name;    }
-    public int getAge() {       return age; }
-    public String getPid() {        return pid; }
-    public void setName(String name) {      this.name = name;   }
-    public void setAge(int age) {       this.age = age; }
-    public void setPid(String pid) {        this.pid = pid; }
-
-    public Person(String name, int age, String pid) {
-        this.name = name;
-        this.age = age;
-        this.pid = pid;
+    public String getName() {
+        return name;
+    }
+    public int getAge() {
+        return age;
     }
 
-    public Person() {   }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public Person() {
+    }
 
     @Override
     public String toString() {
-        return getName() + "\n" + getAge() + "\n" + getPid();
+        return getName() + "\n" + getAge() ;
     }
 }
