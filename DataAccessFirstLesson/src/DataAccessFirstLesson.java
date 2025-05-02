5b284eb3f775798d786a5be1e72733b3d6fd6bc7
ecaterina99 <tricolicikatea1999@gmail.com>
2025-05-02 12:18:47 +0300

    add methods insertPerson & readPersons

--- DataAccessFirstLesson/src/DataAccessFirstLesson.java
+++ DataAccessFirstLesson/src/DataAccessFirstLesson.java
@@ -2,6 +2,8 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class DataAccessFirstLesson {
     public static void main(String[] args) throws IOException {
@@ -10,7 +12,6 @@ public class DataAccessFirstLesson {
         fw.write(person.toString() + "\n");
         fw.close();
     }
-
         Person person = null;
         FileReader fr = new FileReader("myFile.txt");
         StringBuilder stringBuilder = new StringBuilder();
@@ -24,11 +25,10 @@ public class DataAccessFirstLesson {
             person = new Person(userProps[0], Integer.valueOf(userProps[1]), userProps[2]);
         }
         System.out.println(person);
-
          */
 
 
-        Person person = new Person("Bob Ross", 33);
+       // Person person = new Person("Maria D.", 25);
         try (Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/test", "root", "123123");
         ) {
             System.out.println("Connected to database ");
@@ -40,17 +40,51 @@ public class DataAccessFirstLesson {
             st.execute(query);
              */
 
-            String query =  "insert into persons (name, age) values(?, ?)";
+            // Use interface prepared statement
+          /*  String query =  "insert into persons (name, age) values(?, ?)";
             PreparedStatement ps = conn.prepareStatement(query);
             ps.setString(1, person.getName());
             ps.setInt(2, person.getAge());
             ps.execute();
 
+           */
+
+            // insertPerson(conn, person);
+            readPersons(conn);
+
 
         } catch (SQLException e) {
-            System.out.println("Error in database connection" + e.getMessage());
+            System.out.println("Error in database connection " + e.getMessage());
+        }
+    }
+
+    public static void insertPerson(Connection conn, Person person) throws SQLException {
+
+       /* Statement st = conn.createStatement();
+        String query = "insert into persons values(null,'" + person.getName() + "', " + person.getAge() + ")";
+        st.execute(query);
+        */
+        String query = "insert into persons (name, age) values(?, ?)";
+        PreparedStatement ps = conn.prepareStatement(query);
+        ps.setString(1, person.getName());
+        ps.setInt(2, person.getAge());
+        ps.execute();
+    }
+
+    public static void readPersons(Connection conn) throws SQLException {
+        List<Person> personList = new ArrayList<>();
+        Statement st = conn.createStatement();
+        st.executeQuery("select * from persons");
+
+        ResultSet rs = st.getResultSet();
+        while (rs.next()) {
+            Person p = new Person(rs.getString("name"), rs.getInt("age"));
+            personList.add(p);
         }
 
+        for (Person p : personList) {
+            System.out.println(p);
+        }
     }
 
 }
