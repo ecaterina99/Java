6a8a625b5fee07d57b8e3ce84424155fe47b82b4
ecaterina99 <tricolicikatea1999@gmail.com>
2025-05-27 10:48:02 +0300

    used annotation @WebServlet; created index.html

--- Java_ee_demo/src/main/java/HelloServlet.java
+++ Java_ee_demo/src/main/java/HelloServlet.java
@@ -1,17 +1,33 @@
 import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.time.LocalDateTime;
 
-public class HelloServlet extends HttpServlet{
+@WebServlet(urlPatterns = {"/HelloServlet"}, description = "add two numbers")
+public class HelloServlet extends HttpServlet {
 
     void processRequest(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.setContentType("text/html;charset=UTF-8");
         PrintWriter out = response.getWriter();
-        String msg = LocalDateTime.now().toString();
+        // String msg = LocalDateTime.now().toString();
+
+        String msg = null;
+
+        try {
+            int a, b, c;
+            a = Integer.parseInt(request.getParameter("a"));
+            b = Integer.parseInt(request.getParameter("b"));
+            c = a + b;
+
+            msg = a + " + " + b + " = " + c;
+        } catch (NumberFormatException e) {
+            msg = e.getMessage();
+        }
 
         out.println("<html><body><h2>");
         out.println(msg);
@@ -19,12 +35,15 @@ public class HelloServlet extends HttpServlet{
 
     }
 
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-processRequest(req, resp);    }
+        processRequest(req, resp);
+    }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        processRequest(req, resp);
     }
 
     @Override
