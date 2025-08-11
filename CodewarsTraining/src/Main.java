d338c077986e260a649ef5452289a587c2b468d9
ecaterina99 <tricolicikatea1999@gmail.com>
2025-08-11 11:38:35 +0300

    use method String.contains()

--- CodewarsTraining/src/Main.java
+++ CodewarsTraining/src/Main.java
@@ -1,6 +1,9 @@
+
 public static void main(String[] args) {
     String convertString = "GCAT";
     dnaToRna(convertString);
+    girlOrBoy("XX");
+    
 }
 
 //Use method replaceAll
@@ -8,5 +11,22 @@ public static void dnaToRna(String dna) {
     dna = dna.replaceAll("[T]", "U");
     System.out.println(dna);
 }
+
+//Use String.contains
+public static void girlOrBoy(String sperm) {
+    String answer = "";
+
+  /*  if(sperm.contains("Y")) {
+        answer="Congratulations! You're going to have a son.";
+    }else {
+        answer = "Congratulations! You're going to have a daughter.";
+    }
+    System.out.println(answer);
+
+   */
+    System.out.println("Congratulations! You're going to have a "
+            + (sperm.contains("Y") ? "son." : "daughter."));
+}
+
 
 
