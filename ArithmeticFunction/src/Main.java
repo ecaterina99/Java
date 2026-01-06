c8a67f656f35938b6ffe3780f68c83cc7a1b0fbb
ecaterina99 <tricolicikatea1999@gmail.com>
2026-01-06 15:27:31 +0200

    arithmetic function

---
+++ ArithmeticFunction/src/Main.java
@@ -0,0 +1,36 @@
+/**
+ * Given two numbers and an arithmetic operator (the name of it, as a string),
+ * return the result of the two numbers having that operator used on them.
+ * a and b will both be positive integers, and a will always be the first number in the operation,
+ * and b always the second. The four operators are "add", "subtract", "divide", "multiply".
+ */
+
+public class Main {
+    public static void main(String[] args) {
+
+        int a = 9;
+        int b = 3;
+        String operator = "add";
+        int c;
+        switch (operator) {
+            case "add":
+                c = a + b;
+                System.out.println(c);
+                break;
+            case "subtract":
+                c = a - b;
+                System.out.println(c);
+                break;
+            case "multiply":
+                c = a * b;
+                System.out.println(c);
+                break;
+            case "divide":
+                c = a / b;
+                System.out.println(c);
+                break;
+            default:
+                throw new IllegalArgumentException("Invalid argument: " + operator);
+        }
+        }
+    }
